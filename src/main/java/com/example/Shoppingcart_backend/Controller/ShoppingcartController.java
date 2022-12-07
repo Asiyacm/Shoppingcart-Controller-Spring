ca1a334fed9d8bce4f5a66448c0f78082a6e8678
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-07 21:05:04 +0530

    router commit

--- src/main/java/com/example/Shoppingcart_backend/Controller/ShoppingcartController.java
+++ src/main/java/com/example/Shoppingcart_backend/Controller/ShoppingcartController.java
@@ -1,7 +1,24 @@
 package com.example.Shoppingcart_backend.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShoppingcartController {
+
+    @PostMapping("/add")
+    public String Addpage(){
+        return "Welcome to add page";
+    }
+
+    @GetMapping("/view")
+    public String Viewpage(){
+        return "Welcome to view page";
+    }
+
+    @PostMapping("/search")
+    public String Serachpage(){
+        return "Welcome to search page";
+    }
 }
