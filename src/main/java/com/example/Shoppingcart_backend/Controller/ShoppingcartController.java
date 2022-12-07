6be3e921ecdab6855abce87d79c60c652fa9e46c
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-08 00:33:35 +0530

    retrive commit

--- src/main/java/com/example/Shoppingcart_backend/Controller/ShoppingcartController.java
+++ src/main/java/com/example/Shoppingcart_backend/Controller/ShoppingcartController.java
@@ -3,10 +3,9 @@ package com.example.Shoppingcart_backend.Controller;
 import com.example.Shoppingcart_backend.Model.Shoppingcart;
 import com.example.Shoppingcart_backend.dao.ShoppingcartDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ShoppingcartController {
@@ -14,18 +13,19 @@ public class ShoppingcartController {
     @Autowired
     private ShoppingcartDao dao;
 
+    @CrossOrigin(origins = "*")
     @PostMapping(path="/add",consumes = "application/json",produces = "application/json")
     public String Addpage(@RequestBody Shoppingcart s){
         System.out.println(s.getName().toString());
         dao.save(s);
         return "Welcome to add page";
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/view")
-    public String Viewpage(){
-        return "Welcome to view page";
+    public List<Shoppingcart> Viewpage(){
+        return (List<Shoppingcart>) dao.findAll();
     }
-
+    @CrossOrigin(origins = "*")
     @PostMapping("/search")
     public String Serachpage(){
         return "Welcome to search page";
